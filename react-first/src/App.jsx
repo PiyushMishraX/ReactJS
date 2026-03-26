414f25f8fe47ec5ec550989eb3257563c99087b0
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-26 23:15:25 +0530

    error case in rafce/ any

--- react-first/src/App.jsx
+++ react-first/src/App.jsx
@@ -1,5 +1,21 @@
-function App(){
-  return 'hello'
-}
-export default App
+// function App(){
+//   // return 'hello'
+//   return <h1>hello</h1>
+// }
+// export default App
 // exporting the app to main jsx as deafult 
+
+// rafce - react arrow fn export component
+import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>App</h1>
+//       <h2>App</h2>
+//     </div>
+//     // <h3>give error </h3>
+//     // return "hello" 10 // do not works // one return at a time  // pass inisde div only 
+//   )
+// }
+
--- react-first/src/main.jsx
+++ react-first/src/main.jsx
@@ -3,7 +3,7 @@
 // import './index.css'
 // import App from './App.jsx'
 
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -15,4 +15,5 @@ import App from './App.jsx'
 // )
 
 
+ReactDOM.createRoot(document.getElementById('root')).render(App())
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
