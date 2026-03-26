839fff2c072626c18685aceabbce9ad9f792fd51
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-26 21:33:35 +0530

    fn calling in react js

--- Basics/JSX/script.js
+++ Basics/JSX/script.js
@@ -6,3 +6,11 @@ h1.innerHTML = 'Hello from JS '
 console.log(h1);
 
 document.body.appendChild(h1)
+
+// fn
+
+function abc() {
+    console.log('hello')
+}
+
+abc()
--- Basics/JSX/script.jsx
+++ Basics/JSX/script.jsx
@@ -1,3 +1,17 @@
 // mixture of js and html
 
 var h1 = <h1>Hello</h1> // create element i jsx
+
+// function abc() {
+//     console.log('hello')
+// }
+
+// abc()
+// <abc/>
+
+function Hero() {
+    console.log('hello')
+}
+
+Hero()
+<Hero />
