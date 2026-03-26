d9af3975f590d75d7eb5446322efaa9e5510ffbe
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-26 23:17:31 +0530

    understanding returned structured

--- react-first/src/App.jsx
+++ react-first/src/App.jsx
@@ -19,3 +19,13 @@ import React from 'react'
 //   )
 // }
 
+const App = () => {
+  return (
+    <div id='parent'>
+      <h1 id='child1'>App</h1>
+      <h2 id='child2'>App</h2>
+    </div> 
+  )
+}
+
+export default App
