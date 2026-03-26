b59d48070eea693f46c949527ad9959ffbe56431
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-27 00:03:47 +0530

    create and use component

--- Components(02)/src/App.jsx
+++ Components(02)/src/App.jsx
@@ -1,4 +1,22 @@
 import React from 'react'
+import Card from './components/card'
+
+
+// not good we should try to kee[ App()] empty
+// const App = () => {
+//   return (
+//     <div>
+//       <div className="card">
+//         <h1>Piyush Mishta</h1>
+//         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, amet?</p>
+//       </div>
+        // <div className="card">
+        //  //         <h1>Piyush Mishta</h1>
+        // //         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, amet?</p>
+        // </div>
+//     </div>
+//   )
+// }
 
 const App = () => {
   return (
@@ -7,6 +25,7 @@ const App = () => {
         <h1>Piyush Mishta</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, amet?</p>
       </div>
+      {Card()}
     </div>
   )
 }
