af97f556e63b1a18de3ce72d0f83164130c63df3
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-27 16:23:19 +0530

    show variable value in html using {}

--- Components(02)/src/App.jsx
+++ Components(02)/src/App.jsx
@@ -18,16 +18,31 @@ import Card from './components/card'
 //   )
 // }
 
+// variableprinitng is possible through {var_name}
 const App = () => {
+  const user = 'Piyush'
+  const age = 20
   return (
     <div>
       <div className="card">
-        <h1>Piyush Mishta</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, amet?</p>
+        {/* <h1>user</h1> -- commenting iside react html */} 
+        <h1>{user}</h1>
+        <h1>hello I am {age} years old</h1>
       </div>
-      {Card()}
     </div>
   )
 }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <div className="card">
+//         <h1>Piyush Mishta</h1>
+//         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, amet?</p>
+//       </div>
+//       {Card()}
+//     </div>
+//   )
+// }
 
 export default App
