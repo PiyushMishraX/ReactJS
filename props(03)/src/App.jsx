46e6de53c524de8077f18d1e9c8acda58ece2b58
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-27 23:26:50 +0530

    multiple cards

--- props(03)/src/App.jsx
+++ props(03)/src/App.jsx
@@ -9,6 +9,12 @@ const App = () => {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
         <button>View Profile</button>
       </div>
+      <div className="card">
+        <img src="https://plus.unsplash.com/premium_photo-1674583794341-50bb046c49a7?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <h1>Piyush Mishra</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        <button>View Profile</button>
+      </div>
     </div>
   )
 }
