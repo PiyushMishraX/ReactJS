e5d13998d111bfb9f9a1d157e2a1dcc16848e31e
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-27 23:26:24 +0530

    image addition in carde adn changes

--- props(03)/src/App.jsx
+++ props(03)/src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div className='parent'>
       <div className="card">
+        <img src="https://plus.unsplash.com/premium_photo-1674583794341-50bb046c49a7?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D" alt="" />
         <h1>Piyush Mishra</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
         <button>View Profile</button>
