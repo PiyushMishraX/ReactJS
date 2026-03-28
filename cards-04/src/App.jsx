32e4cc2b6e3373d4f91e638f7f4cf78170d2cfce
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-28 16:52:26 +0530

    full card ui

--- cards-04/src/App.jsx
+++ cards-04/src/App.jsx
@@ -1,40 +1,45 @@
-import React from 'react'
-import {Bookmark} from 'lucide-react'
+import React from "react";
+import { Bookmark } from "lucide-react";
 // import {RiBookmarkLine} from '@remixicon/react/HomeFillIcon';
 
 const App = () => {
   return (
-      <div className="parent">
-
-        <div className="card">
-
+    <div className="parent">
+      <div className="card">
+        <div>
           <div className="top">
-            <img src="https://thumbs.dreamstime.com/b/icons-sample-psost-setting-428651108.jpg" alt="" />
+            <img
+              src="https://thumbs.dreamstime.com/b/icons-sample-psost-setting-428651108.jpg"
+              alt=""
+            />
             {/* <button>save <Bookmark size="{12}" " /></button> */}
-            <button>save <i className="ri-bookmark-line"></i></button>
+            <button>
+              save <i className="ri-bookmark-line"></i>
+            </button>
           </div>
 
           <div className="center">
-            <h3>Amazon <span>5 days ago</span></h3>
+            <h3>
+              Amazon <span>5 days ago</span>
+            </h3>
             <h2>Senior Ui/UX designer</h2>
-            <div className='tag'>
+            <div className="tag">
               <h4>Part Time</h4>
               <h4>Senior Level</h4>
             </div>
-  
-          </div>
-
-          <div className="bottom">
-              <div>
-                <h3>$120/hr</h3>
-                <p>Mumbai, India</p>
-              </div>
-              <button>Apply Now</button>  
           </div>
         </div>
 
+        <div className="bottom">
+          <div>
+            <h3>$120/hr</h3>
+            <p>Mumbai, India</p>
+          </div>
+          <button>Apply Now</button>
+        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
