ec1d57998c0e87690721ac9ef8fbe1117a78e3b6
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-28 21:55:38 +0530

    problem with normal way

--- css-05/src/components/Header.jsx
+++ css-05/src/components/Header.jsx
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <div className='header'>
       <h3>Amazon</h3>
-      <button>Login</button>
+      {/* <button>Login</button> */}
+      <button className='btn'>Login</button>
+      {/* mistakenlly gave btn class to button which applies asme properties as the btn class , which we do not want */}
     </div>
   )
 }
