ef4f77e6871e94bd261c19ccaed5e704eae84a55
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-28 15:39:25 +0530

    props

--- props(03)/src/App.jsx
+++ props(03)/src/App.jsx
@@ -40,9 +40,10 @@ const App = () => {
   return (
     <div className='parent'> 
            {/* Card(Piyush 2)  */}
-      <Card user='Piyush 2' age1='20' age2={20}/>  
+      <Card user='Piyush' age1='20' age2={20} img='https://plus.unsplash.com/premium_photo-1674583794341-50bb046c49a7?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D'/>  
       {/* for number array variable etc use {} */}
-      <Card user='Piyush-3' age1='20' age2={20}/> 
+      <Card user='Aditya' age1='20' age2={24} img='https://images.unsplash.com/photo-1622737133809-d95047b9e673?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8M2QlMjByZW5kZXJ8ZW58MHx8MHx8fDA%3D'/> 
+      <Card user='aman' age1='27' age2={20}  img='https://images.unsplash.com/photo-1634848860108-6d8368f5a0b3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8M2QlMjByZW5kZXJ8ZW58MHx8MHx8fDA%3D'/> 
     </div>
   )
 }
--- props(03)/src/components/Card.jsx
+++ props(03)/src/components/Card.jsx
@@ -21,6 +21,27 @@ import React from "react";
 //   );
 // };
 
+// const Card = (props) => {
+
+
+//     console.log(props)
+//     console.log(props.user,props.age1)
+//   return (
+//     // <div>
+//     //   Card
+//     // </div>
+//     <div className="card">
+//       <img
+//         src="https://plus.unsplash.com/premium_photo-1674583794341-50bb046c49a7?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D"
+//         alt=""
+//       />
+//       <h1>{props.user},{props.age2}</h1>
+//       <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+//       <button>View Profile</button>
+//     </div>
+//   );
+// };
+
 const Card = (props) => {
 
 
@@ -32,10 +53,11 @@ const Card = (props) => {
     // </div>
     <div className="card">
       <img
-        src="https://plus.unsplash.com/premium_photo-1674583794341-50bb046c49a7?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D"
+        // src="{props.img}"
+        src={props.img}
         alt=""
       />
-      <h1>{props.user}</h1>
+      <h1>{props.user}, {props.age2}</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
       <button>View Profile</button>
     </div>
