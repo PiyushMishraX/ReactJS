7b0ba85fc49f4fa9b51a0711c08a31d7bcb7ebe0
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-28 22:42:17 +0530

    .module.css extension

--- css-05/src/components/header/Header.jsx
+++ css-05/src/components/header/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 // import './Header.module.css'
 import styles from './Header.module.css'
+// import styles2 from './header_btn.css'
+// it will bnot work bc z modular css uses extension .module.css
 
 const Header = () => {
   return (
     <div className={styles.header}>
       <h3>Amazon</h3>
-      <button className='btn'>Login</button>
+      {/* <button className='btn'>Login</button> */}
+      {/* <button className={styles.btn}>Login</button>  */}
+      {/*  now it only apply css from inside the styles css only  */}
+
+      {/* <button className={styles2.btn}>Login</button>  */}
     </div>
   )
 }
