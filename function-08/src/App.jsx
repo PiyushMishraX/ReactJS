65a0acbc4d7cbf1b3b2d5a3cd57808320cbe1da0
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-29 22:51:50 +0530

    create function inside the event listerner directly

--- function-08/src/App.jsx
+++ function-08/src/App.jsx
@@ -12,13 +12,25 @@ const App = () => {
   }
   return (
     <div>
-      {/* <button onClick={btnClicked()}> Button </button> no like this because fn() runs it before hands */}
+      {/* <button onClick={btnClicked()}> Button </button> no like this because fn() runs it before hands  whitout click*/}
       {/* <button onClick={btnClicked}> Button </button> */}
 
       {/* <button  onDoubleClick={btnClicked}> Button </button> */}
       {/* runs on double click only */}
 
-      <button onMouseEnter={mouseEnter} onClick={btnClicked}> Button </button>
+      {/* <button onMouseEnter={mouseEnter} onClick={btnClicked}> Button </button> */}
+
+
+      {/* <button onClick={btnClicked}> Button </button>
+      <button onClick={btnClicked}>explore this</button> */}
+
+        <button onClick={function () {
+          console.log('button is clicked')
+
+        }}> Click here </button>
+
+
+
     </div>
   )
 }
