5128dd3e83d395f1e71fbdcb2c4557aaa707cdd4
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-29 17:19:23 +0530

    sizing of icons

--- ui-project-07/src/components/Section1/LeftContent.jsx
+++ ui-project-07/src/components/Section1/LeftContent.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { RiArrowRightUpLine } from '@remixicon/react';
+// import 'remixicon/fonts/remixicon.css';
+
+
 
 const LeftContent = () => {
   return (
-    <div className='h-full w-1/3 bg-blue-600'>
-      Left Content
+    <div className='h-full w-1/4 bg-blue-600'>
+      <div>
+
+      </div>
+      <div className='text-8xl'>
+        {/* <RiArrowRightUpLine /> it overrides any css property given abive bcz it is set  to 96px or size-24 */}
+        {/* <i className="ri-arrow-right-up-line"></i> */}
+
+        {/* <RiArrowRightUpLine className="size-24 text-white" /> class name method */}
+
+        <RiArrowRightUpLine size={96} /> {/* prop method */}
+      </div>
     </div>
   )
 }
