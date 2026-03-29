e4a2be95259dc7ed73c8a543931099cdbec9c75b
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-29 16:16:59 +0530

    navbar

--- ui-project-07/src/components/Section1/Section1.jsx
+++ ui-project-07/src/components/Section1/Section1.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Section1 = () => {
   return (
-    <div className='h-screen w-full bg-blue-950'>
-      Section 1
+    // <div className='h-screen w-full bg-blue-950'>
+    <div className='h-screen w-full '>
+      <Navbar/>
     </div>
   )
 }
