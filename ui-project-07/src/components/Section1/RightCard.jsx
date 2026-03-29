14ce3cc1a687570b281aee942e90494f95460bda
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-29 18:53:27 +0530

    Card ui full

--- ui-project-07/src/components/Section1/RightCard.jsx
+++ ui-project-07/src/components/Section1/RightCard.jsx
@@ -7,19 +7,19 @@ const RightCard = () => {
     <div className="h-full w-70  rounded-4xl overflow-hidden relative">
       <img
         className="h-full w-full object-cover"
-        src="https://images.unsplash.com/photo-1665436035665-d7dad9086ee2?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d29ya2luZyUyMHByb2Zlc3Npb25hbHN8ZW58MHx8MHx8fDA%3D"
+        src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d29ya2luZyUyMHByb2Zlc3Npb25hbHxlbnwwfHwwfHx8MA%3D%3D"
         alt=""
       />
-      <div className=" absolute top-0 left-0 h-full w-full bg-amber-300 p-8 flex flex-col justify-between ">
-        <h2 className=" bg-white rounded-full h-14 w-14 flex justify-center items-center text-2xl font-bold">1</h2>
+      <div className=" absolute top-0 left-0 h-full w-full p-6 flex flex-col justify-between ">
+        <h2 className=" bg-white rounded-full h-11 w-11 flex justify-center items-center text-xl font-bold">1</h2>
         <div>
-          <p>
+          <p className="text-lg leading-normal text-white mb-10">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
             consequatur ab maiores laudantium, itaque nisi.
           </p>
-          <div>
-            <button>Satisfied</button>
-            <button>
+          <div className=" flex justify-between">
+            <button className="bg-blue-600 text-white font-medium px-8 py-2 rounded-full ">Satisfied</button>
+            <button className="bg-blue-600 text-white font-medium px-3 py-2 rounded-full " >
               <RiArrowRightLine />
             </button>
           </div>
--- ui-project-07/src/components/Section1/RightContent.jsx
+++ ui-project-07/src/components/Section1/RightContent.jsx
@@ -3,7 +3,8 @@ import RightCard from './RightCard'
 
 const RightContent = () => {
   return (
-    <div className='h-full w-2/3 bg-blue-300 p-6 '>
+    // <div className='h-full w-2/3 bg-blue-300 p-6 '>
+    <div className='h-full w-2/3 bg-blue-300 px-5 py-4 '>
       <RightCard/>
     </div>
   )
