1104fd89c8dd90e94815a3f5099ddea72534babd
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-30 17:35:54 +0530

    use state for string

--- useState-09/src/App.jsx
+++ useState-09/src/App.jsx
@@ -21,16 +21,28 @@ const App = () => {
   const [num, setNum] = useState(0) // let/const num doesn't change in view
 
   function changeNum() {
+    // num is read only, setNum write only 
     // num = 30;// no
-    setNum(30);
+    setNum(30); // only changed part is reloaded 
+    // num is constant 
+    console.log(num);
+    // the value of num is changed after the whole function is run so for first time num prints zero
+
+
+    setUsername("vijay")
+    
   }
+
+  // string 
+  const [username, setUsername] = useState('Piyush')
   
   return (
     <div>
       {/* <h1>Value of a is {a}</h1>
       <button onClick={changeA} >Click</button> */}
 
-      <h1>Value of num is {num}</h1>
+      <h1>Value of num is {num}</h1><br />
+      <h1>Value of user is {username}</h1>
       <button onClick={changeNum}>Click</button>
     </div>
   )
