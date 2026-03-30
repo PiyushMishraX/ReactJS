a57b4608a18a1508b9c5a77f1e5355aea3986540
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-30 23:49:11 +0530

    notes app ui edit

--- note-app-13/src/App.jsx
+++ note-app-13/src/App.jsx
@@ -3,22 +3,29 @@ import React from "react";
 const App = () => {
   return (
     <div className="h-screen bg-black rtext-white  ">
-      <form className=" flex p-10  gap-4  ">
+      <form className=" flex p-10 justify-between  gap-4  ">
         <div className="items-start w-1/2 flex flex-col gap-4 ">
-        <input
-          type="text"
-          placeholder="Enter Notes Heading"
-          className="p-5 w-1/2 border-2 py-2 rounded-2xl"
-        />
-        <input
-          type="text"
-          placeholder="Write details"
-          className="p-5 w-1/2 h-20 py-2 border-2 rounded-2xl"
-        />
-        <button className= "w-1/2 bg-white text-black px-5 py-2 rounded-2xl">Add Note </button>
+          <input
+            type="text"
+            placeholder="Enter Notes Heading"
+            className=" w-full p-5 border-2 py-2 rounded outline-none "
+          />
+          <textarea
+            type="text"
+            placeholder="Write details"
+            className=" w-full p-5 h-20 py-2 border-2 rounded outline-none "
+          />
+          <button className="w-full bg-white text-black px-5 py-2 rounded  outline-none ">
+            Add Note{" "}
+          </button>
         </div>
 
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZqbO68IB-_p9J2rJf5BIWWkHX8yX-OZ0k5g&s" alt="" />
+        {/* <img
+        // className="h-60"
+        className=" rotate-y-180 h-60"
+          src="https://static.vecteezy.com/system/resources/thumbnails/049/578/155/small/a-black-and-white-drawing-of-a-man-writing-png.png"
+          alt=""
+        /> */}
       </form>
     </div>
   );
