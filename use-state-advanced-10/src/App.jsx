b5da92486f15e0c61b331b88d4a3aef4df49c75a
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-30 18:37:17 +0530

    setNum(num)

--- use-state-advanced-10/src/App.jsx
+++ use-state-advanced-10/src/App.jsx
@@ -3,14 +3,18 @@ import React, { useState } from "react";
 const App = () => {
     const [num, setNum] = useState(10)
 
-    const btnClicked = ()=>{
-      // console.log('button clicked')
+    // const btnClicked = ()=>{
+    //   // console.log('button clicked')
 
-      console.log(num); // 10 for first click
+    //   console.log(num); // 10 for first click
       
-      // setNum(20) // it is asynchronus 
-      setNum(num+1) // it is asynchronus 
-      console.log(num);  // still 10 after click  // ui updates but it does not bcz ui takes some time to update but log runs right after
+    //   // setNum(20) // it is asynchronus 
+    //   setNum(num+1) // it is asynchronus 
+    //   console.log(num);  // still 10 after click  // ui updates but it does not bcz ui takes some time to update but log runs right after
+    // }
+
+    const btnClicked = ()=>{
+      setNum(num) // react doesn't rerender , react identifies if same value is entered if yes not rerendering 
     }
   return (
     <div>
