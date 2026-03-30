387eab79d46e951374a5151311ce5dbc3659b1d0
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-30 23:53:05 +0530

    add form handling

--- note-app-13/src/App.jsx
+++ note-app-13/src/App.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 
 const App = () => {
+
+
+  // form handling 
+  const submitHandler= (e)=>{
+    e.preventDefault()
+    console.log('Form submittes');
+  }
+
   return (
     <div className="h-screen bg-black rtext-white  ">
-      <form className=" flex p-10 justify-between  gap-4  ">
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }} className=" flex p-10 justify-between  gap-4  ">
         <div className="items-start w-1/2 flex flex-col gap-4 ">
           <input
             type="text"
             placeholder="Enter Notes Heading"
-            className=" w-full p-5 border-2 py-2 rounded outline-none "
+            className=" w-full p-5 border-2 py-2 rounded outline-none font-medium "
           />
           <textarea
             type="text"
             placeholder="Write details"
-            className=" w-full p-5 h-20 py-2 border-2 rounded outline-none "
+            className=" w-full p-5 h-32 py-2 border-2 rounded outline-none font-medium "
           />
-          <button className="w-full bg-white text-black px-5 py-2 rounded  outline-none ">
+          <button className="w-full bg-white text-black px-5 py-2 rounded  outline-none font-medium ">
             Add Note{" "}
           </button>
         </div>
