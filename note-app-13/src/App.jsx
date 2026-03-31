935732b1e2ae61290a4022b0db505567891136bf
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-31 23:11:30 +0530

    backgroun dimage

--- note-app-13/src/App.jsx
+++ note-app-13/src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [title, setTitle] = useState('')
-  const [details, setDetails] = useState('')
-  
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
   // mapping
-  const [task, setTask] = useState([])
-  
+  const [task, setTask] = useState([]);
+
   // form handling
   const submitHandler = (e) => {
     e.preventDefault();
@@ -17,22 +17,18 @@ const App = () => {
 
     const copyTask = [...task]; // old task copies
 
-    copyTask.push({title,details})
-    setTask(copyTask)
+    copyTask.push({ title, details });
+    setTask(copyTask);
 
     console.log(task); // use batch update to see the changed value from set task / if it works
     console.log(copyTask);
-    
 
-    setTitle('')
-    setDetails('')
+    setTitle("");
+    setDetails("");
   };
-
-
 
   return (
     <div className="h-screen lg:flex bg-black text-white   ">
-
       <form
         onSubmit={(e) => {
           submitHandler(e);
@@ -47,25 +43,24 @@ const App = () => {
           placeholder="Enter Notes Heading"
           className=" w-full p-5 border-2 py-2 rounded outline-none font-medium "
           value={title}
-          onChange={(e)=>{
+          onChange={(e) => {
             // console.log(e.target.value);
-            setTitle(e.target.value)
+            setTitle(e.target.value);
           }}
-          />
+        />
 
-          {/* deatails input */}
+        {/* deatails input */}
         <textarea
           type="text"
           placeholder="Write details Here"
           className=" w-full p-5 h-32 py-2 border-2 rounded outline-none font-medium "
           value={details}
-          onChange={(e)=>{
-            setDetails(e.target.value)
+          onChange={(e) => {
+            setDetails(e.target.value);
           }}
         />
 
-        <button className="w-full bg-white text-black px-5 py-2 rounded  outline-none font-medium cursor-pointer active:bg-gray-200  active:scale-95 "
-        >
+        <button className="w-full bg-white text-black px-5 py-2 rounded  outline-none font-medium cursor-pointer active:bg-gray-200  active:scale-95 ">
           Add Note{" "}
         </button>
 
@@ -77,17 +72,28 @@ const App = () => {
         /> */}
       </form>
 
-      <div className="lg:w-1/2 gap-5 lg:border-l-2 flex-wrap p-10">
-        <h1 className=" text-4xl font-bold    ">Recent Notes</h1>
-        <div className=" flex  flex-wrap gap-5 mt-5 h-full overflow-auto">
+      <div className="lg:w-1/2 lg:border-l-2  p-10">
+        <h1 className="text-4xl font-bold">Recent Notes</h1>
+        <div className="flex flex-wrap items-start justify-start gap-5 mt-6 h-[90%] overflow-auto">
           {/* <div className="h-52 w-40 rounded-2xl bg-white"></div> */}
-          
-          {task.map(function(elem, idx){
+
+          {task.map(function (elem, idx) {
             // return 'Hello'
-            return <div key='idx' className="h-52 w-40 rounded-2xl bg-white p-4 text-black ">
-              <h3 className=" leading-tight text-xl font-bold " >{elem.title}</h3>
-              <p className="mt-2 leading-tight font-medium text-gray-500" >{elem.details}</p>
-            </div>
+            // return <div key='idx' className="h-52 w-40 rounded-2xl bg-white p-4 text-black ">
+            return (
+              <div
+                key="idx"
+                className=" relative bg-cover h-52 w-40 rounded-2xl p-4 text-black  bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')] "
+              >
+                <h2></h2>
+                <h3 className=" leading-tight text-xl font-bold ">
+                  {elem.title}
+                </h3>
+                <p className="mt-2 leading-tight font-medium text-gray-500">
+                  {elem.details}
+                </p>
+              </div>
+            );
           })}
         </div>
       </div>
