6c80b7bd7d6684ad9711a28389a129074a9cd7e4
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-01 23:55:36 +0530

    useeffect reacting

--- use-effect-16/src/App.jsx
+++ use-effect-16/src/App.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
 import { useEffect } from 'react';
 
 const App = () => {
+
+  const [num, setNum] = useState(0)
 
   function random() {
     // const a = Math.random()
     // console.log('hello');// ye task main task jaisa nahi chalana hai , side stack mai chalana hai
 
     useEffect(() => {
-      log('use effect i running')
-  })
+    console.log('use effect is running');
+  }, [num]);
     
     
     
   }
   return (
-    <div onClick={random}>App</div>
+    // <div onClick={random} >App</div>
+    <div >
+      <h1>{num}</h1>
+      <button onClick={()=>{
+        // setNum(10)
+        setNum(num+1)
+      }}>click</button>
+    </div>
   )
 }
 
