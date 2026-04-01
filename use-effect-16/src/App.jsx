827ad68ecb371d81fadea7a1733598e6b73cdb5c
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-01 23:46:57 +0530

    useeffect syntax

--- use-effect-16/src/App.jsx
+++ use-effect-16/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useEffect } from 'react';
 
 const App = () => {
+
+  function random() {
+    // const a = Math.random()
+    // console.log('hello');// ye task main task jaisa nahi chalana hai , side stack mai chalana hai
+
+    useEffect(() => {
+      log('use effect i running')
+  })
+    
+    
+    
+  }
   return (
-    <div>App</div>
+    <div onClick={random}>App</div>
   )
 }
 
