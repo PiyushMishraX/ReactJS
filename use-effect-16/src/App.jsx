d1f8064b282eda940c68e0fd505edee3974bdf72
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-02 22:17:47 +0530

    final learning use effect

--- use-effect-16/src/App.jsx
+++ use-effect-16/src/App.jsx
@@ -1,37 +1,114 @@
-import React from 'react'
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+// const App = () => {
+//   const [num, setNum] = useState(0);
+//   const [num2, setnum2] = useState(100);
+
+//   // function random() {
+//   // const a = Math.random()
+//   // console.log('hello');// ye task main task jaisa nahi chalana hai , side stack mai chalana hai
+
+//   // mousinting for setNum(num+1)
+//   //   useEffect(() => {
+//   //   console.log('use effect is running');
+//   // });
+
+//   // }
+
+//   // console.log("1. Component is rendering...");
+
+//   // useEffect(() => {
+//   //   console.log("2. useEffect fired! (Side Stack)");
+//   // }, [num]);
+
+//   // dependencies
+//   useEffect(() => {
+//     console.log("use effect is running");
+//   }, [num]); //depend on num
+//   // }, []); // no dependies changes wherever an eleent change
+
+//   return (
+//     // <div onClick={random} >App</div>
+//     <div>
+//       <h1> value of num is {num}</h1>
+//       <h1> value of num2 is {num2}</h1>
+//       <button 
+//       //   onClick={() => {
+//       //     // setNum(10)
+//       //     setNum(num + 1);
+//       //     // setnum2(num2 + 1);
+
+      
+//       //   }}
+//       // onDoubleClick={()=>{
+//       //   setnum2(num2 + 1);
+//       // }}  
+//         onMouseEnter={() => {
+//           // setNum(10)
+//           setNum(num + 1);
+//           // setnum2(num2 + 1);
+
+      
+//         }}
+//       onMouseLeave={()=>{
+//         setnum2(num2 + 1);
+//       }}  
+//       >
+//         click
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
 
 const App = () => {
 
-  const [num, setNum] = useState(0)
+  const [a, setA] = useState(0)
+  const [b, setB] = useState(0)
 
-  function random() {
-    // const a = Math.random()
-    // console.log('hello');// ye task main task jaisa nahi chalana hai , side stack mai chalana hai
-
-
-    // mousinting for setNum(num+1)
-  //   useEffect(() => {
-  //   console.log('use effect is running');
-  // });
-
-  // dependencies
-    useEffect(() => {
-    console.log('use effect is running');
-  }, [num]);
-    
-    
-    
+  function aChanging(){
+    console.log('value of A changes');
   }
+  function bChanging(){
+    console.log('value of B changes');
+  }
+   
+  // useEffect(() => {
+  //   console.log("use effect is running...");
+  // })
+
+  // useEffect(() => {
+  //   console.log("use effect is running...");
+  // },[]) // have dependecies which is '' empty string a value assigned to this changes the it will run
+
+  useEffect(() => {
+    console.log("use effect is running...");
+  // },[a])
+  // },[b])
+  },[a,b])
+  
+
   return (
-    // <div onClick={random} >App</div>
-    <div >
-      <h1>{num}</h1>
-      <button onClick={()=>{
-        // setNum(10)
-        setNum(num+1)
-      }}>click</button>
+    <div>
+      <h1>A is {a}</h1>
+      <h1>B is {b}</h1>
+      <button
+      onClick={()=>{
+        setA(a+1)
+      }}>Change A</button>
+
+      <button
+      onClick={()=>{
+        setB(b+1)
+      }}>Change B</button>
     </div>
   )
 }
