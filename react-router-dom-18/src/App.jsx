c28d80584c5460f5f20e1b638c1bcc19d7054058
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-03 20:27:44 +0530

    non changing nav

--- react-router-dom-18/src/App.jsx
+++ react-router-dom-18/src/App.jsx
@@ -2,15 +2,35 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
     <div>
+      {/* <h2>This is navbar</h2>  */}
+      {/* above will show in every page */}
+
+      <div className='nav'>
+
+        <h3>Website</h3>
+        <div>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+        </div>
+      </div>
+
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} /> 
         {/* react router dom se single page application ( SPA ) bana hai , no reloading and url changes */}
+
+        <Route path='/contact' element={<Contact />} /> 
+
       </Routes>
+
+      <h2>This is  footer</h2>
+      {/* see in every page */}
     </div>
   )
 }
