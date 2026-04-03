56fe7109e4e334b8b743f0e499bf73e6e66416c8
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-03 17:06:26 +0530

    links ( anchor )

--- gallery-project-17/src/App.jsx
+++ gallery-project-17/src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
 
   const [userData, setUserData] = useState([])
+  const [index, setIndex] = useState(1)
 
   const getData = async ()=>{
     // console.log(("data"));
@@ -17,7 +18,7 @@ const App = () => {
 
     // const response = await axios.get('https://picsum.photos/v2/list')/
     // const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=30') // no same value
-    const response = await axios.get('https://picsum.photos/v2/list?page=3&limit=30') // no same value
+    const response = await axios.get('https://picsum.photos/v2/list?page=3&limit=15') // no same value
 
     // console.log(response)
     // console.log(response.data)
@@ -29,35 +30,54 @@ const App = () => {
     
   }
 
-  let printUserData = "No user available"
+  useEffect(function () {
+    getData() // loading on sidestack without clickking
+  },[])
+
+  // let printUserData = "No user available"
+  let printUserData = <h3 className='text-gray-400 text-xs'>No User Available</h3>
 
   if(userData.length > 0){
     printUserData = userData.map(function (elem,idx) {
       // return 'hello'
       // return idx
       // console.log(elem.download_url)
-      return <div>
+      return <div key={idx} >
 
-      {/* <div className='h-40 w-44 overflow-hidden bg-white rounded-xl' > */}
-      <div className='h-40 w-44 overflow-hidden  rounded-xl' >
+      <a href={elem.url} target='_blank'>
+        {/* <div className='h-40 w-44 overflow-hidden bg-white rounded-xl' > */}
+        <div className='h-40 w-44 overflow-hidden  rounded-xl' >
         {/* <img src="elem.download_url" alt="" /> // use curly braces */}
         <img className='h-full w-full object-cover ' src={elem.download_url} alt="" />
       </div>
-      <h2>{elem.author}</h2>
+      <h2 className=' font-bold text-lg'>{elem.author}</h2>
+      </a>
+      
       </div>
     })
   }
 
   return (
     <div className='bg-black h-screen p-4 overflow-auto text-white'>
-      <button
+      {/* <button
         onClick={getData}
       className='bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded text-white'>
         get data
-      </button>
+      </button> */}
 
-      <div className='flex flex-wrap gap-3'> 
-    {printUserData}
+      <div className='flex flex-wrap gap-4 p-2'> 
+          {printUserData}
+      </div>
+
+      <div className='flex gap-6 justify-center items-center '>
+        <button 
+        className=' bg-amber-400 text-black rounded px-4 py-2 font-semibold text-sm cursor-pointer active:scale-95 '>
+          Prev
+          </button>
+        <button 
+        className=' bg-amber-400 text-black rounded px-4 py-2 font-semibold text-sm cursor-pointer active:scale-95 '>
+          Next
+          </button>
       </div>
       
     </div>
