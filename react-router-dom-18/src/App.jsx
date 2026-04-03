13352f4c121cbfaa646cc065a6181ead9179bf29
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-03 19:41:56 +0530

    different routers

--- react-router-dom-18/src/App.jsx
+++ react-router-dom-18/src/App.jsx
@@ -13,3 +13,20 @@ export default App
 // routing -> switching pages
 
 // react do not have inuilt routing so we use react router dom library
+
+// browser router
+
+// routes 
+// website.com = / // main page
+// website.com/courses = /courses
+// website.com/about = /about
+
+// hash router routes - it is not sent to server
+// website.com = /#/                        // main page
+// website.com/courses = /#/courses
+// website.com/about = /#/about
+
+//memiey outer
+// data stored in memory unlike in browser router stroin gin history // used mostly in react native 
+
+// static router next js
