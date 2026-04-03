9a6a23ef1b7b09e787c0147b3165d6327ee607a4
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-03 21:29:10 +0530

    navabr

---
+++ routing-advanced-19/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-black text-white '>
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
