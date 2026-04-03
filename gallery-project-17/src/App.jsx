df641da02a9da0c924250a4904735440d4dbe82e
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-03 17:38:02 +0530

    creating component

--- gallery-project-17/src/App.jsx
+++ gallery-project-17/src/App.jsx
@@ -1,110 +1,64 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Card from './compoents/Card'
 
 const App = () => {
-
   const [userData, setUserData] = useState([])
   const [index, setIndex] = useState(1)
 
-  const getData = async ()=>{
-    // console.log(("data"));
-
-    // axios
-    // axios.post('url',{username:'Piyush', password:'piyushihere'})
-    // axios.patch // update
-    // axios.delete // 
-
-
-
-    // const response = await axios.get('https://picsum.photos/v2/list')/
-    // const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=30') // no same value
-    // const response = await axios.get('https://picsum.photos/v2/list?page=3&limit=15') 
-
-    // for multiple pages // use backtick or template literal
-    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=15`) 
-
-    // console.log(response)
-    // console.log(response.data)
-    
+  const getData = async () => {
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`)
     setUserData(response.data)
-    console.log(userData);
-
-    console.log(response.data);
-    
   }
 
   useEffect(function () {
-    getData() // loading on sidestack without clickking
-  // },[]) 
-  },[index]) // now it will be called whenever index reloads
+    getData()
+  }, [index])
 
-  // let printUserData = "No user available"
-  // let printUserData = <h3 className='text-gray-400 text-xs'>No User Available</h3>
-  let printUserData = <h3 className='text-gray-400 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 '>Loading</h3>
+  let printUserData = (
+    <h3 className='text-gray-300 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold '>
+      Loading....
+    </h3>
+  )
 
-  if(userData.length > 0){
-    printUserData = userData.map(function (elem,idx) {
-      // return 'hello'
-      // return idx
-      // console.log(elem.download_url)
-      return <div key={idx} >
-
-      <a href={elem.url} target='_blank'>
-        {/* <div className='h-40 w-44 overflow-hidden bg-white rounded-xl' > */}
-        <div className='h-40 w-44 overflow-hidden  rounded-xl' >
-        {/* <img src="elem.download_url" alt="" /> // use curly braces */}
-        <img className='h-full w-full object-cover ' src={elem.download_url} alt="" />
-      </div>
-      <h2 className=' font-bold text-lg'>{elem.author}</h2>
-      </a>
-      
-      </div>
+  if (userData.length > 0) {
+    printUserData = userData.map(function (elem, idx) {
+      return (
+        <div key={idx}>
+          <Card elem={elem}/>
+        </div>
+      )
     })
   }
 
   return (
     <div className='bg-black h-screen p-4 overflow-auto text-white'>
-
-      <h1 className=' fixed bg-red-500 text-6xl'>{index}</h1> 
-      {/* <button
-        onClick={getData}
-      className='bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded text-white'>
-        get data
-      </button> */}
-
-      <div className='flex flex-wrap gap-4 p-2'> 
-          {printUserData}
+      <div className='flex h-[94%] flex-wrap gap-4 p-2'>
+        {printUserData}
       </div>
 
-      <div className='flex gap-6 justify-center items-center '>
-        <button 
-        className=' bg-amber-400 text-black rounded px-4 py-2 font-semibold text-sm cursor-pointer active:scale-95 '
-        onClick={()=>{
-          // console.log("Prev button clicked");
-          if(index>1){
-            setIndex(index-1)
-            // lazy loading to insure smoothness even if the images aren't loaded
-            setUserData([]) // now images will first be cleared than next will be loaded
-
-          }
-          
-        }}
+      <div className='flex gap-6 justify-center items-center'>
+        <button
+          className='bg-amber-400 text-black rounded px-4 py-2 font-semibold text-sm cursor-pointer active:scale-95'
+          onClick={() => {
+            if (index > 1) {
+              setIndex(index - 1)
+              setUserData([])
+            }
+          }}
         >
           Prev
-          </button>
-        <button 
-        className=' bg-amber-400 text-black rounded px-4 py-2 font-semibold text-sm cursor-pointer active:scale-95 '
-        onClick={()=>{
-          // console.log("Next button clicked");
-          setIndex(index+1)
-          setUserData([]) 
-          
-        }}
+        </button>
+        <button
+          className='bg-amber-400 text-black rounded px-4 py-2 font-semibold text-sm cursor-pointer active:scale-95'
+          onClick={() => {
+            setIndex(index + 1)
+            setUserData([])
+          }}
         >
           Next
-          </button>
+        </button>
       </div>
-      
     </div>
   )
 }
---
+++ gallery-project-17/src/compoents/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Card = (props) => {
+
+    console.log(props); 
+    console.log(props.elem); 
+    // data is indside props.elem because props is object with each passed value as a property such as elem and elem has the data
+
+  return (
+    <div>
+        <a href={props.elem.url} target='_blank' rel="noreferrer">
+            <div className='h-50 w-55 overflow-hidden rounded-xl'>
+              <img className='h-full w-full object-cover' src={props.elem.download_url} alt={props.elem.author} />
+            </div>
+            <h2 className='font-bold text-lg'>{props.elem.author}</h2>
+          </a>
+    </div>
+  )
+}
+
+export default Card
