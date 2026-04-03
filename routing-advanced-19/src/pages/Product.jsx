cd876e6ad1ca0761fe8e08459fcd9479581bda65
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-03 22:54:49 +0530

    nested page

--- routing-advanced-19/src/pages/Product.jsx
+++ routing-advanced-19/src/pages/Product.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Product = () => {
   return (
     <div>
-        <h1>Product page</h1>
+      <div className="flex justify-center gap-10 py-4 bg-red-600 ">
+        <Link className="text-xl font-semibold" to="/product/men">
+          Men
+        </Link>
+        <Link className="text-xl font-semibold" to="/product/women">
+          WoMen
+        </Link>
+      </div>
+      <h1>Product page</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
