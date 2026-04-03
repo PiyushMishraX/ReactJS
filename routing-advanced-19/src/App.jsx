c15853151aff414e8cdbc94962674bee76b44516
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-03 21:47:49 +0530

    add routing

--- routing-advanced-19/src/App.jsx
+++ routing-advanced-19/src/App.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { Route , Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className='h-screen bg-black text-white '>
+    <div className="h-screen bg-black text-white ">
       <Navbar />
+      {/* <About /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- routing-advanced-19/src/components/Navbar.jsx
+++ routing-advanced-19/src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className=' flex py-4 px-8 items-center bg-cyan-800 justify-between'>
-      <h1 className='text-xl font-bold '>Website</h1>
-      <div className='flex gap-8'>
-        <a className='text-lg font-bold ' href="/">Home</a>
-        <a className='text-lg font-bold ' href="/about">About</a>
-        <a className='text-lg font-bold ' href="/contact">Contact</a>
+      <h2 className='text-xl font-bold '>Website</h2>
+      <div className='flex gap-10'>
+        <a className='text-medium font-bold ' href="/">Home</a>
+        <a className='text-medium font-bold ' href="/about">About</a>
+        <a className='text-medium font-bold ' href="/contact">Contact</a>
       </div>
     </div>
   )
