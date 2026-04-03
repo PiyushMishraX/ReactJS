5299666db3c2f05fb45a46fb7725798099822f22
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-04 00:06:20 +0530

    navigate to home and previous page (-1)

--- routing-advanced-19/src/pages/About.jsx
+++ routing-advanced-19/src/pages/About.jsx
@@ -7,14 +7,21 @@ const About = () => {
 
     const navigate = useNavigate()
 
-    const btnClicked = ()=>{
-        // console.log('button is clicked');
-        
-    }
+    // const btnClicked = ()=>{
+    //     // console.log('button is clicked');
+    //     navigate('/') // the page goes to home page
+    // }
 
     return (
     <div>
-        <button onClick={btnClicked} className=' bg-emerald-400 px-5 py-2 rounded m-2 cursor-pointer active:scle-95'>Return to Homepage</button>
+        {/* <button onClick={btnClicked} className=' bg-emerald-400 px-5 py-2 rounded m-2 cursor-pointer active:scle-95'>Return to Homepage</button> */}
+
+        <button onClick={()=>{
+            navigate('/')
+        }} className=' bg-emerald-400 px-5 py-2 rounded m-2 cursor-pointer active:scle-95'>Return to Homepage</button>
+        <button onClick={()=>{
+            navigate(-1) // goes to the previous page such as product etc not just home 
+        }} className=' bg-emerald-400 px-5 py-2 rounded m-2 cursor-pointer active:scle-95'>Return to Back</button>
         <h1>About Page</h1>
     </div>
   )
