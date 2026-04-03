0969c134f351db1a51945a5f8b790ce7ca74c7d0
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-03 19:01:54 +0530

    react router dom

---
+++ react-router-dom-18/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
+
+// routing -> switching pages
+
+// react do not have inuilt routing so we use react router dom library
