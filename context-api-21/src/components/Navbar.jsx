a361f79e29be8496de1ed05f6f380d5e9a2d8c72
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-04 16:58:12 +0530

    use destructuring of props

--- context-api-21/src/components/Navbar.jsx
+++ context-api-21/src/components/Navbar.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
 import Nav2 from './Nav2'
 
-const Navbar = (props) => {
-    console.log(props);
-    console.log(props.children);
-    console.log(props.children[0]);
+// const Navbar = (props) => {
+//     console.log(props);
+//     console.log(props.children);
+//     console.log(props.children[0]);
+    
+//   return (
+//     <div className='nav'>
+//         <h2>Website</h2>
+//         {/* <Nav2 {theme={props.theme}} /> */}
+//         {props.children[0]}
+//         {props.children[1]}
+//         {/* it is basic concept of passing elements
+//         thorugh children */}
+
+//         <Nav2 theme={props.theme} />
+//     </div>
+//   )
+// }
+
+// props is an array we can directly use children and theme properties which is being passed , using destructuring // so we can use desired properties only which we help to reduce conflicts and errors
+// const Navbar = ({children}) => {
+const Navbar = ({children,theme}) => {
+    console.log(children);
     
   return (
     <div className='nav'>
         <h2>Website</h2>
-        {/* <Nav2 {theme={props.theme}} /> */}
-        {props.children[0]}
-        {props.children[1]}
-        {/* it is basic concept of passing elements
-        thorugh children */}
-
-        <Nav2 theme={props.theme} />
+        <Nav2 theme={theme} />
     </div>
   )
 }
