782a8a2a2d64f4609d7375880bea12fedd6e2c8d
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-04 17:12:04 +0530

    creating context

--- context-api-21/src/context/ThemeContext.jsx
+++ context-api-21/src/context/ThemeContext.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { createContext } from "react";
+
+// creating context-
+
+//  ThemeDataContext is the context which will provide the data in eveywhere
+export const ThemeDataContext = createContext();
+// export so any where it can be used 
 
 const ThemeContext = (props) => {
+
   return (
     // <div>ThemeContext</div>
-    <div>
-        {props.children}
-    </div>
-  )
-}
+    <div>{props.children}</div>
+  );
+};
 
-export default ThemeContext
+export default ThemeContext;
 
 // now wrap app with the context so everything iside app and even app have the the values so this vlaue is in boudry of app sourouding everthing
--- context-api-21/src/main.jsx
+++ context-api-21/src/main.jsx
@@ -1,22 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import ThemeContext from './context/ThemeContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ThemeContext from "./context/ThemeContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-
-    // <App />
+createRoot(document.getElementById("root")).render(
+  // <App />
 
   <ThemeContext>
+    {/* <h1>Hello </h1> */}
     <App />
-  </ThemeContext>
-)
+  </ThemeContext>,
+);
 
-// it is context wrapping 
+// it is context wrapping
 // // but now the theme context will show instead of all
-// it happens because of using <></> , self closign tags do not have more tags inside so they are not problem but tags inside <></> will not show 
+// it happens because of using <></> , self closign tags do not have more tags inside so they are not problem but tags inside <></> will not show
 
 // so we sue {props.children} to show the children which is app
 
 // we write theme context like routing same as routing wrapping we can use theme context in all pages now by wrapping it
+
+
+// now follow three steps 
