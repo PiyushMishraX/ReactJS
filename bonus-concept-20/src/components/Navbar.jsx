463399230514c4787dba4f8d664243cac910a71e
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-04 15:29:59 +0530

    props reverse final commit

--- bonus-concept-20/src/components/Navbar.jsx
+++ bonus-concept-20/src/components/Navbar.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 
 const Navbar = (props) => {
     // console.log(props)
+
+    function changeTheme(){
+        // console.log('Theme change', props.theme); // light
+        console.log('Theme change');
+        props.setTheme('Dark') // set dark in parent // because setTheme created in parent
+        
+    }
+
   return (
     <div>
         {/* <p>{props.theme}</p> */}
-        <button onClick={()=>{
+        {/* <button onClick={()=>{
             props.setTheme('Dark')
-        }}>Change theme</button>
+        }}>Change theme</button> */}
+        <button onClick={changeTheme}>Change theme</button>
     </div>
   )
 }
